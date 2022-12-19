357276a7f9a30dc2c9c4e0231789ef9bab0994c4
Armon Gardner <a123gardner@gmail.com>
2022-12-18 18:26:37 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const One = document.getElementById("1");
+const Two = document.getElementById("2");
+const Three = document.getElementById("3");
+const Four = document.getElementById("4");
+const Five = document.getElementById("5");
+const Six = document.getElementById("6");
+const Seven = document.getElementById("7");
+const Eight = document.getElementById("8");
+const Nine = document.getElementById("9");
+const Zero = document.getElementById("0");
+console.log(Zero);
+Zero.onclick = function (e) {
+  console.log("zero has been pressed");
+};
